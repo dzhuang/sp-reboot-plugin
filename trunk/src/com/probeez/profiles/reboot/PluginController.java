3d3117fe0cbf3156cc873fe292fc7a4a6f5433ec
slava.probeez@gmail.com <slava.probeez@gmail.com@81712f21-2427-e789-d0bb-541f8c2e8697>
2011-03-06 21:01:11 +0000

    Added countdown timer

--- trunk/src/com/probeez/profiles/reboot/PluginController.java
+++ trunk/src/com/probeez/profiles/reboot/PluginController.java
@@ -1,13 +1,11 @@
 package com.probeez.profiles.reboot;
 
-import java.io.IOException;
-
+import static android.content.Intent.FLAG_ACTIVITY_NEW_TASK;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 
 import com.probeez.profiles.plugin.PluginBroadcastReceiver;
-
 /**
  * Class to listen for SP intent broadcasts.<br>
  * It should be declared in <code>AndroidManifest.xml</code> with the intent filter:
@@ -24,20 +22,16 @@ public class PluginController extends PluginBroadcastReceiver {
 	static final String STATE_ACTION = "state_action";
 	static final int ACTION_REBOOT = 0;
 	static final int ACTION_SHUTDOWN = 1;
+
 	@Override
   protected void onPerformAction(Context context, Bundle state, boolean triggered) {
-		if (!triggered) {
-			return;
+		if (triggered) {
+			Intent intent = new Intent(context, RebootTimerActivity.class);
+			int action = state.getInt(STATE_ACTION, ACTION_REBOOT);
+			intent.putExtra(STATE_ACTION, action);
+			intent.setFlags(FLAG_ACTIVITY_NEW_TASK);
+			context.startActivity(intent);
 		}
-		int action = state.getInt(STATE_ACTION);
-		try {
-	    Runtime.getRuntime().exec(new String[] {
-	    	"su", "-c",
-	    	(action==ACTION_REBOOT? "reboot": "reboot -p")
-	    });
-		} catch (IOException e) {
-			Log.e(TAG, "Cannot execute su", e);
-    }
   }
 
 }
